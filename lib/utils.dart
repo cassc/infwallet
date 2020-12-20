d9479cd6137d63ed612c505e3aecbf4d587968e6
Chen Li <chencassc@gmail.com>
2020-12-20 10:21:19 +0800

    add time picker to datetime picker

--- lib/utils.dart
+++ lib/utils.dart
@@ -4,18 +4,28 @@ import 'package:intl/intl.dart';
 
 const DEFAULT_EDGE = 12.0;
 
-Widget datetimePicker(initDate, onChanged){
+Widget datetimePicker(initDate, onChanged) {
   final format = DateFormat("yyyy-MM-dd HH:mm");
-    var df = DateTimeField(
-      format: format,
-      onShowPicker: (context, currentValue) {
-        return showDatePicker(
-            context: context,
-            firstDate: DateTime(2012),
-            initialDate: initDate,
-            lastDate: DateTime(2100));
-      },
-      onChanged: onChanged,
-    );
-    return df;
+
+  var df = DateTimeField(
+    format: format,
+    onChanged: onChanged,
+    onShowPicker: (context, currentValue) async {
+      final date = await showDatePicker(
+          context: context,
+          firstDate: DateTime(2012),
+          initialDate: initDate ?? DateTime.now(),
+          lastDate: DateTime(2100));
+      if (date != null) {
+        final time = await showTimePicker(
+          context: context,
+          initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+        );
+        return DateTimeField.combine(date, time);
+      } else {
+        return currentValue;
+      }
+    },
+  );
+  return df;
 }
